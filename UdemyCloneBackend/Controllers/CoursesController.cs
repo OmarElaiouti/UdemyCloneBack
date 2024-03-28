ee89bd8693b1a77654a8fcf3fc862166e41e2e4c
Omar <omarelaiouti@gmail.com>
2024-03-28 23:49:01 +0200

    decode the toke to get user id

--- UdemyCloneBackend/Controllers/CoursesController.cs
+++ UdemyCloneBackend/Controllers/CoursesController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using Udemy.Core.DTOs;
 using Udemy.Core.Interfaces;
 using Udemy.Core.Models;
+using Udemy.EF.Repository;
+using UdemyCloneBackend.Services;
 
 namespace UdemyApi.Controllers
 {
@@ -11,10 +13,13 @@ namespace UdemyApi.Controllers
     public class CoursesController : ControllerBase
     {
         private readonly ICourseRepository _courseService;
+        private readonly IAuthService  _authService;
 
-        public CoursesController(ICourseRepository courseService)
+        public CoursesController(ICourseRepository courseService, IAuthService authService)
         {
             _courseService = courseService;
+            _authService = authService;
+
         }
 
         [HttpGet("searched-courses")]
@@ -45,12 +50,24 @@ namespace UdemyApi.Controllers
             }
         }
 
-        [HttpGet("course-in-cart")]
-        public async Task<ActionResult<List<CourseCardWithRateDto>>> GetCoursesInCartByUserId([FromQuery] string id)
+        [HttpGet("courses-in-cart")]
+        public async Task<ActionResult<List<CourseCardWithRateDto>>> GetCoursesInCartByUserId([FromHeader(Name = "Authorization")] string token)
         {
             try
             {
-                var courses = await _courseService.GetCoursesInCartByUserId(id);
+                string userId = await _authService.DecodeTokenAsync(token.Replace("Bearer ", ""));
+
+                if (string.IsNullOrEmpty(userId))
+                {
+                    return BadRequest("Invalid token or token expired.");
+                }
+
+                var courses = await _courseService.GetCoursesInCartByUserId(userId);
+
+                if (courses == null)
+                {
+                    return NotFound("courses not found in the user cart.");
+                }
                 return Ok(courses);
             }
             catch (Exception ex)
